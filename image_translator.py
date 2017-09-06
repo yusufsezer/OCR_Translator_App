b4af494e3c328e10f109e00880982b629a688ba8
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-06 10:14:18 -0400

    Finished implementation of translate_image_text()

--- image_translator.py
+++ image_translator.py
@@ -1,11 +1,13 @@
 from google.cloud import vision
 from google.cloud import translate
 from PyQt5.QtCore import QByteArray, QBuffer
+import collections
+WordBox = collections.namedtuple('WordBox', ['word', 'geometry'])
 
 
 class ImageTranslator:
 
-    def __init__(self, target_language = 'en'):
+    def __init__(self, target_language='en'):
         self.__vision_client = vision.Client()
         self.__translate_client = translate.Client()
         self.__target_language = target_language
@@ -14,11 +16,24 @@ class ImageTranslator:
         self.__target_language = target_language
 
     def translate_image_text(self, image):
+        # image data to be fed to Google Cloud Vision API
         data = QByteArray()
         buffer = QBuffer(data)
         image.save(buffer, 'JPG')
+
+        # Use Google Cloud Vision API's OCR capabilities to extract text from image
         img_to_translate = self.__vision_client.image(content = data.data())
-        text_to_translate = img_to_translate.detect_text()[0].description
-        print(text_to_translate)
-        translation = self.__translate_client.translate(text_to_translate, target_language=self.__target_language)
-        print(translation)
+        text_to_translate = img_to_translate.detect_text()
+
+        word_boxes = []
+        # Create WordBox namedtuples with the translation and bounding box of each word
+        # Append each WordBox to word_boxes
+        for word_data in text_to_translate[1:]:
+            translated_word = self.__translate_client.translate(word_data.description, target_language=self.__target_language)
+            boundary_vertices = word_data.bounds.vertices
+            box = []
+            for vertex in boundary_vertices:
+                box.append((vertex.x_coordinate, vertex.y_coordinate))
+            word_boxes.append(WordBox(word=translated_word['translatedText'], geometry=box))
+
+        return word_boxes
