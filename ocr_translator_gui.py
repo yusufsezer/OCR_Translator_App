7b59686ff229f090150362c8c063b3893dd086a9
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-06 10:15:32 -0400

    Added target language selection via QComboBox. Minor code refactoring

--- ocr_translator_gui.py
+++ ocr_translator_gui.py
@@ -1,22 +1,24 @@
-# GUI for OCR Text Translator App
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QFrame, QSizePolicy
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QByteArray, QBuffer
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QFrame, QSizePolicy, QComboBox
+from PyQt5.QtGui import QImage
 from image_capture import ImageCapture
 from image_frame import ImageFrame
 from image_translator import ImageTranslator
 
+
 class TranslatorGUI (QWidget):
+
+    language_codes = {'English': 'en', 'Spanish': 'es', 'French': 'fr', 'German': 'de', 'Chinese': 'zh'}
 
     def __init__(self):
         super().__init__()
         self.init_ui()
         self.imageCaptureDelegate = ImageCapture() # Webcam Image Capture Delegate
-        self.translateDelegate = ImageTranslator() # OCR Text-in-Image Translator
+        self.translateDelegate = ImageTranslator() # OCR Text-in-Image Translator Delegate
 
     def init_ui(self):
 
-        # Create layouts and add vertical layouts to the horizontal layout
+        # Create one horizontal and two vertical layouts
+        # Add both vertical layouts to the horizontal layouts
         self.hLayout = QHBoxLayout()
         self.leftVLayout = QVBoxLayout()
         self.rightVLayout = QVBoxLayout()
@@ -28,21 +30,25 @@ class TranslatorGUI (QWidget):
                                    "Use this app to translate the text contained in images!")
         self.welcomeLabel.setWordWrap(True)
 
-        # Create and setup buttons
+        # Create and setup buttons/combobox
         self.takePicBtn = QPushButton("Take a Picture")
-        self.takePicBtn.clicked[bool].connect(self.take_picture)
+        self.takePicBtn.clicked[bool].connect(self.__take_picture)
         self.slctImgBtn = QPushButton("Select an Existing Image")
-        self.slctImgBtn.clicked[bool].connect(self.select_existing_image)
+        self.slctImgBtn.clicked[bool].connect(self.__select_existing_image)
         self.translateImgBtn = QPushButton("Translate Text in Image")
-        self.translateImgBtn.clicked[bool].connect(self.translate_image_text)
+        self.translateImgBtn.clicked[bool].connect(self.__translate_image_text)
+        self.selectTargetLanguageBox = QComboBox()
+        self.selectTargetLanguageBox.addItems(['English', 'Spanish', 'French', 'German', 'Chinese'])
 
         # Add appropriate widgets to the left vertical layout
         self.leftVLayout.addWidget(self.welcomeLabel)
+        self.leftVLayout.addWidget(self.selectTargetLanguageBox)
         self.leftVLayout.addWidget(self.takePicBtn)
         self.leftVLayout.addWidget(self.slctImgBtn)
         self.leftVLayout.addWidget(self.translateImgBtn)
 
         # Create QImage and ImageFrame to display image
+        # Load image into QImage
         self.image = QImage()
         self.imageFrame = ImageFrame()
         self.imageFrame.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
@@ -50,7 +56,7 @@ class TranslatorGUI (QWidget):
         # self.frameBtn.setText("EXAMPLE")
         # self.frameBtn.setGeometry((self.imageFrame.width / 2) - 250,
         #                           (self.imageFrame.width / self.imageFrame.aspectRatio / 2) - 250, 500, 500)
-        self._load_image("default.jpg")
+        self.__load_image("default.jpg")
 
         # Add appropriate widgets to right vertical layout
         self.rightVLayout.addWidget(self.imageFrame)
@@ -60,19 +66,21 @@ class TranslatorGUI (QWidget):
         self.setWindowTitle("OCR Translator App")
         self.show()
 
-    def take_picture(self):
+    def __take_picture(self):
         imageFileName = self.imageCaptureDelegate.capture_image()
-        self._load_image(imageFileName)
+        self.__load_image(imageFileName)
 
-    def select_existing_image(self):
+    def __select_existing_image(self):
         fileDialog = QFileDialog()
         imageFileName = fileDialog.getOpenFileName()
-        self._load_image(imageFileName[0])
+        self.__load_image(imageFileName[0])
 
-    def translate_image_text(self):
-        self.translateDelegate.translate_image_text(self.image)
+    def __translate_image_text(self):
+        self.translateDelegate.set_target_language(TranslatorGUI.language_codes[self.selectTargetLanguageBox.currentText()])
+        word_boxes = self.translateDelegate.translate_image_text(self.image)
+        self.__draw_word_boxes(word_boxes)
 
-    def _load_image(self, fileName):
+    def __load_image(self, fileName):
         self.image.load(fileName)
         imgWidth = self.image.width()
         imgAspectRatio = self.image.width() / self.image.height()
@@ -81,3 +89,6 @@ class TranslatorGUI (QWidget):
         self.imageFrame.setWidth(imgWidth)
         # self.frameBtn.setGeometry((self.imageFrame.width/2)-250,(self.imageFrame.width/self.imageFrame.aspectRatio/2)-250, 500, 500)
         # self.frameBtn.setStyleSheet('border-image: null; background-color: transparent')
+
+    def __draw_word_boxes(self, word_boxes):
+        print(word_boxes)
