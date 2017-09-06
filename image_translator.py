2d4c21f24a6fc47fc16cac7c7cc7aaabee7c2b53
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-06 12:50:10 -0400

    Added Docstrings. Minor code restructuring

--- image_translator.py
+++ image_translator.py
@@ -1,35 +1,43 @@
 from google.cloud import vision
 from google.cloud import translate
-from PyQt5.QtCore import QByteArray, QBuffer
 import collections
 WordBox = collections.namedtuple('WordBox', ['word', 'geometry'])
 
 
 class ImageTranslator:
+    """Handles requests to the Google Cloud Vision and Translate APIs
+
+    :type target_language: str
+    :param target_language: the target language to which text will be translated. If not passed, defaults to 'en',
+                            resulting in translations into English.
+    """
+    __vision_client = vision.Client()
+    __translate_client = translate.Client()
 
     def __init__(self, target_language='en'):
-        self.__vision_client = vision.Client()
-        self.__translate_client = translate.Client()
         self.__target_language = target_language
 
     def set_target_language(self, target_language):
         self.__target_language = target_language
 
-    def translate_image_text(self, image):
-        # image data to be fed to Google Cloud Vision API
-        data = QByteArray()
-        buffer = QBuffer(data)
-        image.save(buffer, 'JPG')
+    def translate_image_text(self, image_data):
+        """Detect and translate each word in the image, and construct a list of WordBox namedtuples.
 
-        # Use Google Cloud Vision API's OCR capabilities to extract text from image
-        img_to_translate = self.__vision_client.image(content = data.data())
+        :type image_data: object
+        :param image_data: pointer to the image data to extract/translate text from
+
+        :rtype: list
+        :returns: a list of WordBox namedtuples. Each WordBox encapsulates a word's translation and the vertices of its
+                  bounding box
+        """
+
+        img_to_translate = ImageTranslator.__vision_client.image(content=image_data)
         text_to_translate = img_to_translate.detect_text()
 
         word_boxes = []
-        # Create WordBox namedtuples with the translation and bounding box of each word
-        # Append each WordBox to word_boxes
         for word_data in text_to_translate[1:]:
-            translated_word = self.__translate_client.translate(word_data.description, target_language=self.__target_language)
+            translated_word = ImageTranslator.__translate_client.translate(word_data.description,
+                                                                           target_language=self.__target_language)
             boundary_vertices = word_data.bounds.vertices
             box = []
             for vertex in boundary_vertices:
