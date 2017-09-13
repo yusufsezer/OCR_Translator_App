7b0c8e2afbebb7aefbbfc7fb14938e476a42c502
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-13 10:10:45 -0400

    partially implemented __draw_word_boxes method

--- ocr_translator_gui.py
+++ ocr_translator_gui.py
@@ -1,8 +1,11 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QFrame, QSizePolicy, QComboBox
-from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QImage, QPolygon, QPolygonF, QRegion, QPainterPath, QPainter, QPen, QBrush
+from PyQt5.QtCore import QByteArray, QBuffer, QPoint
 from image_capture import ImageCapture
 from image_frame import ImageFrame
 from image_translator import ImageTranslator
+# import collections
+# WordBox = collections.namedtuple('WordBox', ['word', 'geometry'])
 
 
 class TranslatorGUI (QWidget):
@@ -77,7 +80,12 @@ class TranslatorGUI (QWidget):
 
     def __translate_image_text(self):
         self.translateDelegate.set_target_language(TranslatorGUI.language_codes[self.selectTargetLanguageBox.currentText()])
-        word_boxes = self.translateDelegate.translate_image_text(self.image)
+
+        data = QByteArray()
+        buffer = QBuffer(data)
+        self.image.save(buffer, 'JPG')
+
+        word_boxes = self.translateDelegate.translate_image_text(data.data())
         self.__draw_word_boxes(word_boxes)
 
     def __load_image(self, fileName):
@@ -91,4 +99,35 @@ class TranslatorGUI (QWidget):
         # self.frameBtn.setStyleSheet('border-image: null; background-color: transparent')
 
     def __draw_word_boxes(self, word_boxes):
-        print(word_boxes)
+        word_box = word_boxes[0]
+        word_vertices = word_box.geometry
+        print(word_box)
+        print(word_vertices)
+        btn = QPushButton()
+        btn.setText(word_box.word)
+        btn.setStyleSheet("background-color: darkBlue; border-style: none")
+        poly = QPolygon()
+        flattened_vertices = []
+        # for point in word_vertices:
+        #     print(point[0])
+        #     flattened_vertices.append(QPoint(point[0], point[1]))
+        print(flattened_vertices)
+        flattened_vertices = [coordinate for li in word_vertices for coordinate in li]
+        print(flattened_vertices)
+        poly.setPoints([4] + flattened_vertices)
+        btn.setMask(QRegion(poly))
+        self.rightVLayout.addWidget(btn)
+        # print(1)
+        # painter = QPainter(self.imageFrame)
+        # painter.begin(self.imageFrame)
+        # print(2)
+        # path = QPainterPath()
+        # print(3)
+        # path.addPolygon(QPolygonF(poly))
+        # print(4)
+        # pen = QPen()
+        # pen.setWidth(45)
+        # painter.
+        # painter.drawEllipse(100.0, 200.0, 800.0, 600.0)
+        # painter.drawPath(path)
+        # print('done')
