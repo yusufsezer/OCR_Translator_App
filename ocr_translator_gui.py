276d5ae272ee1bc86fcbdaa8a14c359a9a7d94f6
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-19 23:28:44 -0400

    Heavily modified to work with ImageView instead of ImageFrame

--- ocr_translator_gui.py
+++ ocr_translator_gui.py
@@ -1,11 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QFrame, QSizePolicy, QComboBox
-from PyQt5.QtGui import QImage, QPolygon, QPolygonF, QRegion, QPainterPath, QPainter, QPen, QBrush
-from PyQt5.QtCore import QByteArray, QBuffer, QPoint
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, QComboBox
+from PyQt5.QtGui import QImage, QPolygon, QRegion
+from PyQt5.QtCore import QByteArray, QBuffer
 from image_capture import ImageCapture
-from image_frame import ImageFrame
+from image_view import ImageView
 from image_translator import ImageTranslator
-# import collections
-# WordBox = collections.namedtuple('WordBox', ['word', 'geometry'])
 
 
 class TranslatorGUI (QWidget):
@@ -50,19 +48,13 @@ class TranslatorGUI (QWidget):
         self.leftVLayout.addWidget(self.slctImgBtn)
         self.leftVLayout.addWidget(self.translateImgBtn)
 
-        # Create QImage and ImageFrame to display image
+        # Create QImage and ImageView to display image
         # Load image into QImage
-        self.image = QImage()
-        self.imageFrame = ImageFrame()
-        self.imageFrame.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
-        # self.frameBtn = QPushButton(self.imageFrame)
-        # self.frameBtn.setText("EXAMPLE")
-        # self.frameBtn.setGeometry((self.imageFrame.width / 2) - 250,
-        #                           (self.imageFrame.width / self.imageFrame.aspectRatio / 2) - 250, 500, 500)
-        self.__load_image("default.jpg")
+        self.image = QImage('cover.png')
+        self.imageView = ImageView(self.image)
 
         # Add appropriate widgets to right vertical layout
-        self.rightVLayout.addWidget(self.imageFrame)
+        self.rightVLayout.addWidget(self.imageView)
 
         # setup and show window
         self.setLayout(self.hLayout)
@@ -70,13 +62,13 @@ class TranslatorGUI (QWidget):
         self.show()
 
     def __take_picture(self):
-        imageFileName = self.imageCaptureDelegate.capture_image()
-        self.__load_image(imageFileName)
+        image_file_name = self.imageCaptureDelegate.capture_image()
+        self.__load_image(image_file_name)
 
     def __select_existing_image(self):
-        fileDialog = QFileDialog()
-        imageFileName = fileDialog.getOpenFileName()
-        self.__load_image(imageFileName[0])
+        file_dialog = QFileDialog()
+        image_file_name = file_dialog.getOpenFileName()
+        self.__load_image(image_file_name[0])
 
     def __translate_image_text(self):
         self.translateDelegate.set_target_language(TranslatorGUI.language_codes[self.selectTargetLanguageBox.currentText()])
@@ -86,48 +78,8 @@ class TranslatorGUI (QWidget):
         self.image.save(buffer, 'JPG')
 
         word_boxes = self.translateDelegate.translate_image_text(data.data())
-        self.__draw_word_boxes(word_boxes)
+        self.imageView.draw_word_boxes(word_boxes)
 
-    def __load_image(self, fileName):
-        self.image.load(fileName)
-        imgWidth = self.image.width()
-        imgAspectRatio = self.image.width() / self.image.height()
-        self.imageFrame.setStyleSheet('border-image: url("%s")' % fileName)
-        self.imageFrame.setAspectRatio(imgAspectRatio)
-        self.imageFrame.setWidth(imgWidth)
-        # self.frameBtn.setGeometry((self.imageFrame.width/2)-250,(self.imageFrame.width/self.imageFrame.aspectRatio/2)-250, 500, 500)
-        # self.frameBtn.setStyleSheet('border-image: null; background-color: transparent')
-
-    def __draw_word_boxes(self, word_boxes):
-        word_box = word_boxes[0]
-        word_vertices = word_box.geometry
-        print(word_box)
-        print(word_vertices)
-        btn = QPushButton()
-        btn.setText(word_box.word)
-        btn.setStyleSheet("background-color: darkBlue; border-style: none")
-        poly = QPolygon()
-        flattened_vertices = []
-        # for point in word_vertices:
-        #     print(point[0])
-        #     flattened_vertices.append(QPoint(point[0], point[1]))
-        print(flattened_vertices)
-        flattened_vertices = [coordinate for li in word_vertices for coordinate in li]
-        print(flattened_vertices)
-        poly.setPoints([4] + flattened_vertices)
-        btn.setMask(QRegion(poly))
-        self.rightVLayout.addWidget(btn)
-        # print(1)
-        # painter = QPainter(self.imageFrame)
-        # painter.begin(self.imageFrame)
-        # print(2)
-        # path = QPainterPath()
-        # print(3)
-        # path.addPolygon(QPolygonF(poly))
-        # print(4)
-        # pen = QPen()
-        # pen.setWidth(45)
-        # painter.
-        # painter.drawEllipse(100.0, 200.0, 800.0, 600.0)
-        # painter.drawPath(path)
-        # print('done')
+    def __load_image(self, file_name):
+        self.image.load(file_name)
+        self.imageView.set_image(self.image)
