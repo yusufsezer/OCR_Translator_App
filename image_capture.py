4d5e7ba15a7feff00ac5a806df08713e635a51f8
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-22 11:57:30 -0400

    Added documentation

--- image_capture.py
+++ image_capture.py
@@ -1,23 +1,34 @@
-import io
 import cv2
 
+
 class ImageCapture:
+    """Handles webcam access and image capture.
+
+    :type index: int
+    :var index: index to prevent saving files with the same name as already-saved files.
+    """
 
     index = 1
 
-    def capture_image(self):
-        vCapture = cv2.VideoCapture(0)
+    def capture_image(self) -> str:
+        """Launches webcam window, allows user to capture image, and saves the image as a .jpg file."
 
-        frameIsAvailable, frame = vCapture.read()
+        :return: the file name of the captured/saved image.
+        """
+        v_capture = cv2.VideoCapture(0)
+
+        frame_is_available, frame = v_capture.read()
+
         key = cv2.waitKey(1)
-        while frameIsAvailable and key != 27:
+        while frame_is_available and key != 27:
             cv2.imshow("Press esc to take picture", frame)
-            frameIsAvailable, frame = vCapture.read()
+            frame_is_available, frame = v_capture.read()
             key = cv2.waitKey(1)
-        vCapture.release()
+        v_capture.release()
         cv2.destroyWindow("Press esc to take picture")
-        imgFileName = "savedImage%s.jpg" % (ImageCapture.index)
-        print(imgFileName)
+
+        img_file_name = "savedImage%s.jpg" % (ImageCapture.index)
         ImageCapture.index += 1
-        cv2.imwrite(imgFileName, frame)
-        return imgFileName
+        cv2.imwrite(img_file_name, frame)
+
+        return img_file_name
