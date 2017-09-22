ea283e9054ea5df228b5d9f50498e20a6924d285
Yusuf Sezer <yusufksezer@hotmail.com>
2017-09-22 11:58:21 -0400

    Minor refactoring of code, modified documentation

--- image_translator.py
+++ image_translator.py
@@ -11,33 +11,37 @@ class ImageTranslator:
     :type target_language: str
     :param target_language: the target language to which text will be translated. If not passed, defaults to 'en',
                             resulting in translations into English.
+
+    :type self.__target_language: str
+    :var self.__target_language: the target language to which text will be translated.
+
+    :type __language_codes: dict {str:str}
+    :var __language_codes: a dictionary that maps languages to their two letter codes.
     """
 
     __vision_client = vision.Client()
     __translate_client = translate.Client()
+    __language_codes = {'English': 'en', 'Spanish': 'es', 'French': 'fr', 'German': 'de', 'Chinese': 'zh', 'Turkish': 'tr'}
 
-    def __init__(self, target_language='en'):
-        self.__target_language = target_language
+    def __init__(self, target_language='English') -> None:
+        self.__target_language = ImageTranslator.__language_codes[target_language]
 
-    def set_target_language(self, target_language):
+    def set_target_language(self, target_language: str) -> None:
         """Set the target language for translation requests sent to Google's Cloud Translate API
 
-        :type target_language: str
-        :param two letter language code for the target language
-
-        :rtype None
+        :param target_language: The target language for the translation.
         """
-        self.__target_language = target_language
 
-    def translate_image_text(self, image_data):
+        self.__target_language = ImageTranslator.__language_codes[target_language]
+
+    def translate_image_text(self, image_data: bytes) -> list:
         """Detect and translate each word in the image, and construct a list of WordBox namedtuples.
 
-        :type image_data: bytes
         :param image_data: pointer to the image data to extract/translate text from
 
-        :rtype: list
         :returns: a list of WordBox namedtuples. Each WordBox encapsulates a word's translation and the vertices of its
                   bounding polygon
+                  ex: [WordBox('word'=str, 'geometry'=[(int, int)], WordBox('word'=str, 'geometry'=[(int, int)])
         """
 
         img_to_translate = ImageTranslator.__vision_client.image(content=image_data)
